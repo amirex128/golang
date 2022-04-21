d3ba49afb7dc6c130bc8c5691e593f8729f9785d
Amirex <amirex128@gmail.com>
2022-04-22 00:57:01 +0430

    add lo package and check amir contains in array

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"github.com/samber/lo"
+)
+
 func main() {
+	contains := lo.Contains([]string{"amir", "fatemeh"}, "amir")
+	fmt.Println(contains)
+
 	sayHello("amir")
 }
 
